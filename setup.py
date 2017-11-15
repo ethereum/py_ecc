ecf098a83c29c14f414800917e315e889e527c84
Piper Merriam <pipermerriam@gmail.com>
2017-11-14 20:30:09 -0700

    1.4.2 Release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='py_ecc',
-    version='1.4.1',
+    version='1.4.2',
     description='Elliptic curve crypto in python including secp256k1 and alt_bn128',
     long_description=readme,
     author='Vitalik Buterin',
