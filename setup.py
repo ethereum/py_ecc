a44381ed33102060a064184c2c903095787cb172
6ug <bugcrowd.yvt@gmail.com>
2018-10-20 06:22:26 +0530

    Dropping python2 support

--- setup.py
+++ setup.py
@@ -38,15 +38,14 @@ setup(
     package_data={'py_ecc': ['py.typed']},
     install_requires=[
     ],
-    python_requires='>=2.7, <4',
+    python_requires='>=3.4, <4',
     extras_require=extras_require,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
