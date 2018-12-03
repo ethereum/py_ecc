8c2dba9e521e488c7382431abb1c5eab5b573b2c
Vitalik Buterin <v@buterin.com>
2018-12-03 08:08:42 -0500

    Added inits for BLS

--- py_ecc/__init__.py
+++ py_ecc/__init__.py
@@ -3,3 +3,5 @@ from __future__ import absolute_import
 from . import secp256k1  # noqa: F401
 from . import bn128  # noqa: F401
 from . import optimized_bn128  # noqa: F401
+from . import bls12_381  # noqa: F401
+from . import optimized_bls12_381  # noqa: F401
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('LICENSE') as f:
 
 setup(
     name='py_ecc',
-    version='1.4.3',
+    version='1.4.4',
     description='Elliptic curve crypto in python including secp256k1 and alt_bn128',
     long_description=readme,
     long_description_content_type='Elliptic curve crypto in python including secp256k1 and alt_bn128',
