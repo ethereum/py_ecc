2b0b09d961eaa803784641190367d4a670aa118e
Piper Merriam <pipermerriam@gmail.com>
2018-12-11 11:51:30 -0700

    Bump version: 1.4.5 → 1.4.6

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open('LICENSE') as f:
 setup(
     name='py_ecc',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='1.4.5',
+    version='1.4.6',
     description='Elliptic curve crypto in python including secp256k1 and alt_bn128',
     long_description=readme,
     long_description_content_type='text/markdown',
