5fe1ed42567ae74fc6ecb8539c9bfc99ce122db8
Chih Cheng Liang <chihchengliang@gmail.com>
2019-03-07 01:19:40 +0800

    add fields to optimized_bn128.__init__

--- py_ecc/optimized_bn128/__init__.py
+++ py_ecc/optimized_bn128/__init__.py
@@ -1,11 +1,13 @@
 from __future__ import absolute_import
 
+from py_ecc.fields import (  # noqa: F401
+    optimized_bn128_FQ as FQ,
+    optimized_bn128_FQP as FQP,
+    optimized_bn128_FQ2 as FQ2,
+    optimized_bn128_FQ12 as FQ12,
+)
 from .optimized_field_elements import (  # noqa: F401
     field_modulus,
-    FQ,
-    FQP,
-    FQ2,
-    FQ12,
 )
 from .optimized_curve import (  # noqa: F401
     add,
