1eadc6f1404c6c2527762993eb4a254f155b250d
Bhargavasomu <somubhargava97@gmail.com>
2019-03-09 00:25:01 +0530

    Remove unused type aliases and update comments

--- py_ecc/typing.py
+++ py_ecc/typing.py
@@ -1,7 +1,6 @@
 from typing import (
     Tuple,
     TypeVar,
-    TYPE_CHECKING,
     Union,
 )
 
@@ -29,32 +28,24 @@ from py_ecc.fields.field_elements import (
     FQ2,
     FQ12,
 )
-
-if TYPE_CHECKING:
-    from py_ecc.fields.optimized_field_elements import (  # noqa: F401
-        FQ as Optimized_FQ,
-        FQP as Optimized_FQP,
-        FQ2 as Optimized_FQ2,
-        FQ12 as Optimized_FQ12,
-    )
+from py_ecc.fields.optimized_field_elements import (
+    FQ as Optimized_FQ,
+    FQP as Optimized_FQP,
+    FQ2 as Optimized_FQ2,
+    FQ12 as Optimized_FQ12,
+)
 
 
-# Types For bn128 module
-# These types are wrt FQ, FQ2, FQ12
-FQPoint2D = Tuple[FQ, FQ]
-FQ2Point2D = Tuple[FQ2, FQ2]
-FQ12Point2D = Tuple[FQ12, FQ12]
-FQPPoint2D = Tuple[FQP, FQP]
-
-FQPoint3D = Tuple[FQ, FQ, FQ]
-FQ2Point3D = Tuple[FQ2, FQ2, FQ2]
-FQ12Point3D = Tuple[FQ12, FQ12, FQ12]
-FQPPoint3D = Tuple[FQP, FQP, FQP]
-
+#
 # These types are wrt Normal Integers
+#
 PlainPoint2D = Tuple[int, int]
 PlainPoint3D = Tuple[int, int, int]
 
+
+#
+# Types for the normal curves and fields
+#
 Field = TypeVar(
     'Field',
     # General
@@ -78,25 +69,16 @@ Point3D = Tuple[Field, Field, Field]
 GeneralPoint = Union[Point2D[Field], Point3D[Field]]
 
 
-# Types For optimized_bn128_module
-# These types are wrt FQ, FQ2, FQ12
-Optimized_FQPoint2D = Tuple["Optimized_FQ", "Optimized_FQ"]
-Optimized_FQ2Point2D = Tuple["Optimized_FQ2", "Optimized_FQ2"]
-Optimized_FQ12Point2D = Tuple["Optimized_FQ12", "Optimized_FQ12"]
-Optimized_FQPPoint2D = Tuple["Optimized_FQP", "Optimized_FQP"]
-
-Optimized_FQPoint3D = Tuple["Optimized_FQ", "Optimized_FQ", "Optimized_FQ"]
-Optimized_FQ2Point3D = Tuple["Optimized_FQ2", "Optimized_FQ2", "Optimized_FQ2"]
-Optimized_FQ12Point3D = Tuple["Optimized_FQ12", "Optimized_FQ12", "Optimized_FQ12"]
-Optimized_FQPPoint3D = Tuple["Optimized_FQP", "Optimized_FQP", "Optimized_FQP"]
-
+#
+# Types For optimized curves and fields
+#
 Optimized_Field = TypeVar(
     'Optimized_Field',
     # General
-    "Optimized_FQ",
-    "Optimized_FQP",
-    "Optimized_FQ2",
-    "Optimized_FQ12",
+    Optimized_FQ,
+    Optimized_FQP,
+    Optimized_FQ2,
+    Optimized_FQ12,
     # bn128
     optimized_bn128_FQ,
     optimized_bn128_FQP,
