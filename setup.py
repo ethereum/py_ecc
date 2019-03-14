e3f23bd419d179387f4c7a6ce4b8afe427ea8373
Vitalik Buterin <v@buterin.com>
2019-03-14 04:24:27 -0500

    Bump version: 1.4.8 → 1.5.0

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open('LICENSE') as f:
 setup(
     name='py_ecc',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='1.4.8',
+    version='1.5.0',
     description='Elliptic curve crypto in python including secp256k1 and alt_bn128',
     long_description=readme,
     long_description_content_type='text/markdown',
