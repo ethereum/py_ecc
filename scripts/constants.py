73c59e2e397ba1e653ca33317e433b6a7c72d33b
Chih Cheng Liang <chihchengliang@gmail.com>
2019-05-28 15:42:56 +0800

    Deterministic key gen, and right upper bound

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,21 +1,20 @@
 from pathlib import Path
 import os
 from py_ecc.optimized_bls12_381 import (
-    field_modulus as q,
+    curve_order as r,
     G1,
     G2,
     add,
     multiply,
     pairing,
 )
-import secrets
 
 
 data_path = Path(os.path.dirname(__file__)) / "fixtures" / "data.json"
 
 
 N_VALIDATORS = 100
-privkeys = [secrets.randbelow(q) for i in range(N_VALIDATORS)]
+privkeys = [pow(2, i, r) for i in range(N_VALIDATORS)]
 
 msg = b'\xab'*32
 domain = 5566
