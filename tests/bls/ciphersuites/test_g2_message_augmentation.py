18f47b35be3b10b369eb7e56b834a9ccb5c91480
Carl Beekhuizen <carl@ethereum.org>
2019-12-20 16:29:43 +0200

    Add full Message Augmentation tests

--- tests/bls/ciphersuites/test_g2_message_augmentation.py
+++ tests/bls/ciphersuites/test_g2_message_augmentation.py
@@ -1,1 +1,35 @@
-# TODO: Write Message Augmentation Tests
+import pytest
+
+from py_ecc.bls import G2MessageAugmentation
+
+@pytest.mark.parametrize(
+    'privkey',
+    [
+        (1),
+        (5),
+        (124),
+        (735),
+        (127409812145),
+        (90768492698215092512159),
+        (0),
+    ]
+)
+def test_sign_verify(privkey):
+    msg = str(privkey).encode('utf-8')
+    pub = G2MessageAugmentation.PrivToPub(privkey)
+    sig = G2MessageAugmentation.Sign(privkey, msg)
+    assert G2MessageAugmentation.Verify(pub, msg, sig)
+
+
+@pytest.mark.parametrize(	
+    'SKs,messages',	
+    [	
+        (range(10), range(10)),	
+    ]	
+)	
+def test_aggregate_verify(SKs, messages):	
+    PKs = [G2MessageAugmentation.PrivToPub(SK) for SK in SKs]	
+    messages = [bytes(msg) + PK for msg, PK in zip(messages, PKs)]	
+    signatures = [G2MessageAugmentation.Sign(SK, msg) for SK, msg in zip(SKs, messages)]	
+    aggregate_signature = G2MessageAugmentation.Aggregate(signatures)	
+    assert G2MessageAugmentation.AggregateVerify(zip(PKs, messages), aggregate_signature)
