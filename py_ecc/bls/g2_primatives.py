94ca0e2d46076b5fc945d90e2e39b6073bfb783f
Piper Merriam <piper@pipermerriam.com>
2020-10-27 12:43:49 -0600

    Update py_ecc/bls/g2_primatives.py
    Co-authored-by: Hsiao-Wei Wang <hwwang156@gmail.com>

--- py_ecc/bls/g2_primatives.py
+++ py_ecc/bls/g2_primatives.py
@@ -1,3 +1,4 @@
 # flake8: noqa
-# alias for backwards compatibility
+# `g2_primatives.py` was a typo of `g2_primitives.py`
+# Make an alias for backward compatibility
 from py_ecc.bls.g2_primitives import *
