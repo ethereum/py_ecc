e1822d73de32c7e6f005b18aceb7a7291a5fcd33
kclowes <kclowes@users.noreply.github.com>
2021-11-18 16:36:18 -0700

    Add python3.9 support

--- setup.py
+++ setup.py
@@ -59,6 +59,8 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
 )
