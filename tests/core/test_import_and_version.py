84c438ae2d2f0d7524ec4ee15bfdd3339ca8d350
pacrob <5199899+pacrob@users.noreply.github.com>
2024-08-22 13:53:48 -0600

    Lazy-load all submodules to reduce overall py_ecc import time

--- tests/core/test_import_and_version.py
+++ tests/core/test_import_and_version.py
@@ -1,4 +1,17 @@
+import importlib
+
+from py_ecc import (
+    _lazy_imports,
+)
+
+
 def test_import_and_version():
     import py_ecc
 
     assert isinstance(py_ecc.__version__, str)
+
+
+def test_import_all_submodules():
+    # test import of all submodules due to use of lazy imports
+    for submod in _lazy_imports.values():
+        importlib.import_module(submod)
